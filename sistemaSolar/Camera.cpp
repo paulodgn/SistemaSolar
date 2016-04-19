0e2e7317fbe77166ce1ff7416c7a0bae6d671fef
paulodgn <paulo.nwork@gmail.com>
2016-04-19 22:43:02 +0100

    merge

--- sistemaSolar/Camera.cpp
+++ sistemaSolar/Camera.cpp
@@ -8,11 +8,38 @@ Camera::Camera()
 	this->x = 0;
 	this->y = 0;
 	this->z = 0;
+	this->forward = 0;
 }
 
-void Camera::Input()
+void Camera::Input(unsigned char key)
 {
+	
+		key = toupper(key);
 
+		if (key == 27) exit(0);
+
+		/*if (key == 'A')
+		{
+			printf("tecla a \n");
+			angle -= 0.2f;
+
+		}
+		if (key == 'S')
+		{
+			printf("tecla s\n");
+			angleVertical += 0.2f;
+		}
+		if (key == 'D')
+		{
+			printf("tecla d\n");
+			angle += 0.2f;
+		}*/
+		if (key == 'W')
+		{
+			printf("tecla w\n");
+			this->forward += 0.1;
+		}
+	
 }
 
 void Camera::Move()
--- sistemaSolar/camera.h
+++ sistemaSolar/camera.h
@@ -1,13 +1,14 @@
-class Camera
+static class Camera
 {
 public:
 	Camera();
 	~Camera();
 	void Update();
-	void Input();
+	void Input(unsigned char key);
 	void Move();
 
 	float x, y, z;
+	float strafe, forward;
 
 private:
 
